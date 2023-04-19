8c005be5f2b3f1577923ba6abe8abcda2d129f10
elyncss <elkhanmn@code.edu.az>
2023-04-19 13:59:25 +0400

    adding logout and useNavigate

--- login/src/pages/login/Login.tsx
+++ login/src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.scss";
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 const Login = (props: any) => {
   const [name, setName] = useState<string>('');
